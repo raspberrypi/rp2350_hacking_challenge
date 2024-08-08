cbac21a03a4f6938541d6bd6c3612d6873245044
Thomas Roth <code@stacksmashing.net>
2024-08-08 13:25:48 -0700

    Lock PAGE2 for picotool writes

--- lock_chip.sh
+++ lock_chip.sh
@@ -19,6 +19,7 @@ picotool otp set OTP_DATA_CRIT1.GLITCH_DETECTOR_SENS 3
 
 # Lock writes to PAGE1
 picotool otp set --raw OTP_DATA_PAGE1_LOCK1 0x101010
+picotool otp set --raw OTP_DATA_PAGE2_LOCK1 0x101010
 
 set +e
 set +v
